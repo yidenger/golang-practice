c270f15a21d3c0714f5c06e00c1642fa8d6a54ab
seth <liyidong@yeezon.com>
2018-07-12 12:13:03 +0800

    store

--- gwp/server_multiple.go
+++ gwp/server_multiple.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -33,6 +34,53 @@ func process(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, r.Form)
 }
 
+func write(w http.ResponseWriter, r *http.Request) {
+	str := `hello golang`
+
+	w.Header().Set("Location", "http://google.com")
+	w.Write([]byte(str))
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Location", "http://www.baidu.com")
+	w.WriteHeader(302)
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type Post struct {
+		name string
+		age int
+	}
+	post := &Post{
+		name: "xiaoming",
+		age: 22,
+	}
+
+	jsonData, err := json.Marshal(post)
+
+	fmt.Println(err);
+
+	w.Write(jsonData)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name: "first_cookie",
+		Value: "Go Web Programming",
+		HttpOnly: true,
+	}
+
+	c2 := http.Cookie{
+		Name: "second_cookie",
+		Value: "Node.js",
+		HttpOnly: true,
+	}
+
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
+}
+
 func main(){
 	server := http.Server{
 		Addr: "127.0.0.1:3080",
@@ -44,6 +92,14 @@ func main(){
 	http.HandleFunc("/body", body)
 
 	http.HandleFunc("/process", process)
+
+	http.HandleFunc("/write", write)
+
+	http.HandleFunc("/redirect", redirect)
+
+	http.HandleFunc("/json", jsonExample)
+
+	http.HandleFunc("/set-cookie", setCookie)
 
 	server.ListenAndServe()
 }
